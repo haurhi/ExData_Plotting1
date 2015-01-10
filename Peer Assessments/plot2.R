005d2a5a453fd98ec37e865674e894e31ba2616f
haruhi <yeahtaoqt@gmail.com>
2015-01-10 21:01:26 +0800

    summited

--- Peer Assessments/plot2.R
+++
@@ -1,9 +0,0 @@
-power <- read.table('household_power_consumption.txt',sep=';',stringsAsFactors=FALSE)[66638:69517,]
-
-png(file = 'plot2.png')
-
-plot(as.numeric(power[,3]),xaxt = 'n', type = 'l', xlab = '', ylab = 'Global Active Power (kilowatts)')
-weekday <- c('Thu', 'Fri', 'Sat')
-axis(1, labels = weekday,  at = c(1, 1440 ,2880))
-
-dev.off()
